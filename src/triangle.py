d575b1175e31c7905aa75200900b6d72083b8ea4
modengann <99417989+modengann@users.noreply.github.com>
2026-08-24 17:34:36 -0400

    Initial commit

---
+++ src/triangle.py
@@ -0,0 +1,13 @@
+'''Module containing functions calculating triangle properties'''
+
+# Enter you module contents here
+__author__ = 'Casey'
+__version__ = '1.0'
+
+
+import math
+def hypothenuse(length1, length2):
+    pass
+    
+def area(side1,side2):
+    pass
